5035cda34fb4cce32b6af3d9923f4ccfc732d18c
Rian Almeida <rianoliveira38@gmail.com>
2024-04-09 23:22:44 -0300

    update project

--- app/constants/Index.cs
+++ app/constants/Index.cs
@@ -1,6 +1,8 @@
 public class CONSTANTS{
     public static List<char> OPC = ['s', 'm', '0'];  //s = secund, m = minut, 0 = exit
+
     public static int Secund_Multiplier = 1; // s = 1
+    
     public static int Minut_Multiplier = 60; // m = 60  
 
 }
